298ff5eb07286e97ddeebc35f4c4d6dee721b9f2
Chamara Ariyarathne <chamaraanu@users.noreply.github.com>
2022-11-18 11:24:46 +1100

    positive and negative tests
    negative test with try, catch

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -15,8 +15,60 @@ beforeEach(async () => {
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: "1000000" });
 });
+
 describe("Lottery Contract", () => {
   it("deploys a contract", () => {
     assert.ok(lottery.options.address);
   });
+
+  it("allows one account to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: '100'  // web3.utils.toWei("0.02", "ether")
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
+  });
+
+  it("allows multiple accounts to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: '100' // web3.utils.toWei("0.02", "ether"),
+    });
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: '100' // web3.utils.toWei("0.02", "ether"),
+    });
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: '100' // web3.utils.toWei("0.02", "ether"),
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length);
+  });
+
+  it('requires a minimum amount of wei to enter', async () => {
+    try {
+        await lottery.methods.enter().send({
+            from: accounts[0], 
+            value: 99
+        });
+        assert(false);
+    } catch (err) {
+        assert(err);
+    }
+  });
+
 });
